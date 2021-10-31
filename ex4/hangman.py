c0a8900c675220789a6865335ad48a46f2e6fba5
eyalmutzary <eyalmu1@gmail.com>
2021-10-31 19:44:12 +0200

    part 1 finished

--- ex4/hangman.py
+++ ex4/hangman.py
@@ -1,6 +1,9 @@
 import hangman_helper
 
+WORDS_LIST = hangman_helper.load_words()
 
+# Update a pattern based on the word and the letter it gets
+# Returns the new pattern
 def update_word_pattern(word, pattern, letter):
     pattern = list(pattern)
     if letter in word:
@@ -18,7 +21,6 @@ def validate_guess(guess):
     return guess.islower() and len(guess) == 1 and guess.isalpha()
 
 
-# Don't forget to remove default values
 def run_single_game(words_list, score):
     word = hangman_helper.get_random_word(words_list)
     pattern = '_' * len(word)
@@ -54,26 +56,29 @@ def run_single_game(words_list, score):
     return score
 
 
-def init_new_round(round=0, score=hangman_helper.POINTS_INITIAL):
+def init_new_round(round=1, score=hangman_helper.POINTS_INITIAL):
     status = {"rounds": round, "score": score}
-    status["score"] = run_single_game(hangman_helper.load_words(),
+    status["score"] = run_single_game(WORDS_LIST,
                                       status["score"])
     return status
 
 
-def main():
-    hangman_helper.load_words()
+def init_game():
     status = init_new_round()
-    while status["score"] > 0:
-        msg = "You played " + status["rounds"] + " rounds, and accumulated "
-        + status["score"]
+    while status["score"] > 0: # => Won
+        msg = "You played " + str(status["rounds"]) + \
+              " rounds, and accumulated " + str(status["score"])
         if hangman_helper.play_again(msg):
-            init_new_round(status["rounds"], status["score"])
-    # else: # => Lost
-    #     another_game = hangman_helper.play_again(
-    #         "Game over. You did " + status["round"] + " round. Play again?")
+            status = init_new_round(status["rounds"] + 1, status["score"])
+        else:
+            return
+    msg = "Game over. You did " + str(status["rounds"]) + " rounds."
+    if hangman_helper.play_again(msg):
+        init_game()
 
 
+def main():
+    init_game()
 
 
 if __name__ == "__main__":

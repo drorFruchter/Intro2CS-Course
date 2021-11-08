d7323b47dda31e91d31c751ecc23ca2ec0e07db1
eyalmutzary <eyalmu1@gmail.com>
2021-11-08 15:01:25 +0200

    q2 almost done

--- ex5_other/cartoonify.py
+++ ex5_other/cartoonify.py
@@ -1,20 +1,37 @@
 from typing import List
 
-
+# tested
 def separate_channels(image: List[List[List[int]]]):
-    new_lst = []
-    for i in range(len(image)):
-        for j in range(len(image[i])):
-            for color in image[i][j]:
-                new_lst.append([[color]])
+    if image == [[[]]]:
+        return image
+    new_lst: List[List[List[int]]] = []
+    for _ in range(len(image[0][0])): # creating channels layer
+        new_lst.append([])
+    for row in range(len(image)):
+        for channel in range(len(image[row][0])): # new row in every channel
+            new_lst[channel].append([])
+        for col in range(len(image[row])):
+            for channel, value in enumerate(image[row][col]):
+                new_lst[channel][row].append(value) # adds value by channel
     return new_lst
 
 
 def combine_channels(channels: List[List[List[int]]]):
-    new_lst: List[List[List[int]]] = [[[]]]
-    for channel in channels:
-        new_lst[0][0].append(channel[0][0])
-    return new_lst
+    if channels == [[[]]]:
+        return channels
+    new_image: List[List[List[int]]] = []
+    new_image.append([])
+    for channel in range(len(channels)):
+        for row in range(len(channels[channel])):
+            for pixel in range(len(channels[channel][row])):
+                if len(new_image[row]) < len(channels[channel][row]):
+                    new_image[row].append([]) # creates a new pixel in the channel
+                for i in range(len(channels)):
+                    new_image[row][pixel].append(channels[i][row][pixel])
+                # print(channels[row][pixel])
+                # for i in range(len(channels[channel][channel])):
+                #     new_image[row][pixel].append(channels[channel][row][i])
+    return new_image
 
 
 def RGB2grayscale(colored_image: List[List[List[int]]]):
@@ -28,5 +45,77 @@ def RGB2grayscale(colored_image: List[List[List[int]]]):
     return new_lst
 
 
-# print(separate_channels(combine_channels([[[1]], [[2]]])))
+def blur_kernel(size: int):
+    result: List[List[int]] = []
+    for _ in range(size):
+        inner_list = []
+        for _ in range(size):
+            inner_list.append(1 / (size**2))
+        result.append(inner_list)
+    return result
+
+
+# Didn't understand the question
+def apply_kernel(image, kernel):
+    pass
+
+
+def check_xy_valid(y: float, x: float):
+    if x > 1:
+        x = 1
+    elif x < 0:
+        x = 0
+    if y > 1:
+        y = 1
+    elif y < 0:
+        y = 0
+    return y,x
+
+
+def bilinear_interpolation(image: List[List[int]], y: float, x: float):
+    y, x = check_xy_valid(y, x)
+    calc = round(image[0][0]*(1-x)*(1-y) + image[1][0]*y*(1-x) + image[0][1]*x*(1-y) + image[1][1]*x*y)
+    return calc
+
+
+# Didn't understand the question
+def resize(image, new_height, new_width):
+    pass
+
+# tested
+def rotate_90(image: List[List[int]], direction: str):
+    new_image: List[List[int]] = []
+    new_row: List[int] = []
+    for i in range(len(image[0])):
+        for j in range(len(image), 0, -1):
+            new_row.append(image[j-1][i])
+        new_image.append(new_row)
+        new_row = []
+    if direction == 'R':
+        return new_image
+    elif direction == 'L':
+        return [row[::-1] for row in new_image[::-1]]
+    return None
+
+
+def get_edges(image, blur_size, block_size, c):
+    pass
+
+
+
+# print(combine_channels(separate_channels([[[]]])))
+print(combine_channels([[[1]], [[2]]]))
+print(combine_channels([[[1, 2]], [[3, 4]]]))
+# print(combine_channels(separate_channels([[[1, 2], [3, 4]], [[5, 6]]])))
 # print(RGB2grayscale ([[[100, 180, 240],[100, 180, 240]]]))
+# print(blur_kernel(3))
+# print(bilinear_interpolation([[0, 64], [128, 255]], 0, 0))
+# print(bilinear_interpolation([[0, 64], [128, 255]], 1, 1))
+# print(bilinear_interpolation([[0, 64], [128, 255]], 0.5, 0.5))
+# print(bilinear_interpolation([[0, 64], [128, 255]], 0.5, 1.5))
+# print(rotate_90([[1, 2, 3], [4, 5, 6]], 'R'))
+# print(rotate_90([[1, 2, 3], [4, 5, 6]], 'L'))
+# print(rotate_90([[1, 2, 3], [4, 5, 6], [1, 9, 6], [7, 2, 6]], 'R'))
+# print(rotate_90([[1, 2, 3, 5, 6, 7], [4, 5, 6, 3, 7, 5]], 'R'))
+# print(rotate_90([[1],[2],[1],[2]], 'R'))
+# print(rotate_90([[1, 2, 3]], 'R'))
---
+++ ex5_other/ex5_test.py
@@ -0,0 +1,26 @@
+import unittest
+from typing import List
+from cartoonify import *
+
+def main():
+    test_seperate_channels()
+    test_rotate_90()
+
+
+def test_rotate_90():
+    assert rotate_90([[1, 2, 3], [4, 5, 6]], 'R') == [[4, 1], [5, 2], [6, 3]]
+    assert rotate_90([[1, 2, 3], [4, 5, 6]], 'L') == [[3, 6], [2, 5], [1, 4]]
+    assert rotate_90([[1, 2, 3], [4, 5, 6], [1, 9, 6], [7, 2, 6]], 'R') == [[7, 1, 4, 1], [2, 9, 5, 2], [6, 6, 6, 3]]
+    assert rotate_90([[1, 2, 3, 5, 6, 7], [4, 5, 6, 3, 7, 5]], 'R') == [[4, 1], [5, 2], [6, 3], [3, 5], [7, 6], [5, 7]]
+    assert rotate_90([[1],[2],[1],[2]], 'R') == [[2, 1, 2, 1]]
+    assert rotate_90([[1, 2, 3]], 'R') == [[1], [2], [3]]
+
+
+def test_seperate_channels():
+    assert separate_channels([[[1, 2]]]) == [[[1]], [[2]]]
+    assert separate_channels([[[1, 2], [3, 4]]]) == [[[1, 3]], [[2, 4]]]
+    assert separate_channels([[[1, 2], [3, 4]], [[5, 6]]]) == [[[1, 3], [5]], [[2, 4], [6]]]
+
+
+if __name__ == '__main__':
+    main()

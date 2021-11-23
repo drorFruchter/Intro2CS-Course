785e37d61598d32f75ed6729307f107e7b6dd062
eyalmutzary <eyalmu1@gmail.com>
2021-11-23 13:54:49 +0200

    small update

--- ex7/ex7.py
+++ ex7/ex7.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Any, List
 from ex7_helper import *
 
 # --------- Part 1 ---------
@@ -29,16 +29,17 @@ def log_mult(x: float, y: int) -> float:
 def is_power(b: int, x: int) -> bool:
     if b == 0 or b == 1 or x == 0 or x == 1:
         return True
-    return check_divisior(b, x, b)
+    return check_divisor(b, x, b)
+
 
 # Used for is_power
-def check_divisior(b: int, x: int, c: int) -> bool:
+def check_divisor(b: int, x: int, c: int) -> bool:
     if x == b:
         return True
     elif b > x:
         return False
     else:
-        return check_divisior(b*c, x, c)
+        return check_divisor(int(log_mult(b,c)), x, c)
 
 
 def reverse(s: str) -> str:
@@ -50,5 +51,41 @@ def reverse(s: str) -> str:
 # --------- Part 2 ---------
 
 
-print(is_power(1, 12))
+def play_hanoi(hanoi: Any, n: int, src: Any, dst: Any, temp: Any):
+    if n == 1:
+        hanoi.move(src, dst)
+        return
+    else:
+        play_hanoi(hanoi, n-1, src, dst, temp)
+        hanoi.move(src, temp)
+        hanoi.move(dst, temp)
+        hanoi.move(src, dst)
+        # play_hanoi(hanoi, n-1, src, dst, temp)
 
+
+
+
+# --------- Part 3 ---------
+
+#
+
+def number_of_ones(n:int) -> int:
+    if n == 0:
+        return 0
+    else:
+        return number_of_ones(n-1) + count_ones(n)
+
+
+def count_ones(n: int) -> int:
+    if n == 0:
+        return 0
+    else:
+        if n%10 == 1:
+            return number_of_ones(n//10) + 1
+        else:
+            return number_of_ones(n//10)
+
+
+
+def compare_2d_lists(l1: List[List[int]], l2: List[List[int]]) -> bool:
+    pass

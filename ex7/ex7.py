d5a2d4d35bba391e52c5ed0278d124a2413de3cd
eyalmutzary <eyalmu1@gmail.com>
2021-11-24 13:26:07 +0200

    fixed 5, added 8

--- ex7/ex7.py
+++ ex7/ex7.py
@@ -3,6 +3,7 @@ from ex7_helper import *
 
 # --------- Part 1 ---------
 
+
 def mult(x: float, y: int) -> float:
     if y == 0:
         return 0
@@ -29,23 +30,27 @@ def log_mult(x: float, y: int) -> float:
 def is_power(b: int, x: int) -> bool:
     if b == 0 or b == 1 or x == 0 or x == 1:
         return True
-    return check_divisor(b, x, b)
+    return is_power_helper(b, x, b)
 
 
-# Used for is_power
-def check_divisor(b: int, x: int, c: int) -> bool:
+def is_power_helper(b: int, x: int, c: int) -> bool:
     if x == b:
         return True
     elif b > x:
         return False
     else:
-        return check_divisor(int(log_mult(b,c)), x, c)
+        return is_power_helper(int(log_mult(b,c)), x, c)
 
 
 def reverse(s: str) -> str:
-    if s == '':
-        return ''
-    return reverse(s[1:]) + append_to_end('', s[0])
+    return reverse_helper(s, len(s), '',)
+
+
+def reverse_helper(s: str, n: int, new_s: str) -> str:
+    if n == 0:
+        return new_s
+    else:
+        return reverse_helper(s, n-1, append_to_end(new_s, s[n-1]))
 
 
 # --------- Part 2 ---------
@@ -54,20 +59,15 @@ def reverse(s: str) -> str:
 def play_hanoi(hanoi: Any, n: int, src: Any, dst: Any, temp: Any):
     if n == 1:
         hanoi.move(src, dst)
-        return
     else:
         play_hanoi(hanoi, n-1, src, dst, temp)
         hanoi.move(src, temp)
         hanoi.move(dst, temp)
-        hanoi.move(src, dst)
-        # play_hanoi(hanoi, n-1, src, dst, temp)
-
-
+        play_hanoi(hanoi, n-1, temp, src, dst)
 
 
 # --------- Part 3 ---------
 
-#
 
 def number_of_ones(n:int) -> int:
     if n == 0:
@@ -86,6 +86,65 @@ def count_ones(n: int) -> int:
             return number_of_ones(n//10)
 
 
-
 def compare_2d_lists(l1: List[List[int]], l2: List[List[int]]) -> bool:
-    pass
+    if len(l1) != len(l2):
+        return False
+    else:
+        return compare_2d_lists_helper(l1, l2, len(l1))
+
+
+def compare_2d_lists_helper(l1: List[List[int]], l2: List[List[int]], index: int) -> bool:
+    if index == 0:
+        return True
+    else:
+        return compare_2d_lists_helper(l1, l2, index-1) \
+               and len(l1[index-1]) == len(l2[index-1]) \
+               and compare_1d_lists(l1[index-1], l2[index-1], index)
+
+
+def compare_1d_lists(l1: List[int], l2: List[int], index: int) -> bool:
+    if index == -1:
+        return True
+    else:
+        if len(l1) == 0 and len(l2) == 0:
+            return True
+        return compare_1d_lists(l1, l2, index-1) and l1[index-1] == l2[index-1]
+
+
+
+
+
+def exp_n_x(n, x):
+    if n < 0:
+        return None
+    elif n == 0:
+        return 1
+    elif n == 1:
+        return x+1
+    elif n > 1:
+        return exp_n_x(n-1, x) + ((x**n) / factorial(n))
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return factorial(n-1) * n
+
+
+
+
+
+# def helper(n, k, path, lst):
+#     if n == 0 and k == 0:
+#         lst.append(path)
+#     if n > 0:
+#         helper(n-1, k, path + "r", lst)
+#     if k > 0:
+#         helper(n, k-1, path + "u", lst)
+#
+#
+# def up_and_right(n, k, lst):
+#     path = ''
+#     helper(n, k, path, lst)
+#     return lst

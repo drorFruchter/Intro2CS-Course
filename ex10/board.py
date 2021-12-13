d7c3724f64137e10adf967904cc052a8ef0167af
eyalmutzary <eyalmu1@gmail.com>
2021-12-13 18:28:28 +0200

    board almost done

--- ex10/board.py
+++ ex10/board.py
@@ -1,20 +1,29 @@
 from apple import Apple
 from bomb import Bomb
 from snake import Snake
+from game_parameters import *
 from typing import List, Dict, Any
 
 class Board:
 
     def __init__(self, width: int, height: int, snake: Snake):
-        self.board = [[None for _ in range(width)] for _ in range(height)]
-        # for i in range(height):
-        #     self.board.append([])
-        #     for _ in range(width):
-        #         self.board[i].append(None)
+        # self.board = {"Size": {"width": width, "height": height},
+        #               "Colors": {"green": [],
+        #                          "red": [],
+        #                          "orange": [],
+        #                          "black":[]},
+        #               "Objects": {"apples": [],
+        #                           "bombs": [],
+        #                           "snake": snake}}
 
-        self.objects: Dict[Any] = {
-                        "apples": [],
-                        "bombs": [],
+        self.board = [[None for _ in range(width)] for _ in range(height)]
+        for i in range(height):
+            self.board.append([])
+            for _ in range(width):
+                self.board[i].append(None)
+        self.objects = {"apples": [],
+                        "bomb": None,
+                        "explosion": None,
                         "snake": snake}
 
     def cell_list(self):
@@ -33,12 +42,39 @@ class Board:
         row, col = self._convert_co_to_index(coordinate)
         return self.board[row][col]
 
+    def _add_item_board(self, item, coordinate):
+        row, col = self._convert_co_to_index(coordinate)
+        self.board[row][col] = item
 
     def draw_board(self):
-        pass
+        for apple in self.objects["apples"]:
+            self._add_item_board(apple, apple.get_location())
+            # what happened if snake ate apple
+        if not self.objects["bombs"]:
+            bomb = self.objects["bombs"]
+            self._add_item_board(bomb, bomb.get_location())
+        # elif not self.objects["bombs"]:
+            # explosion_coordinates =
+        snake_coordinates = self.objects["snake"].snake_coordinates()
+        for coordinate in snake_coordinates:
+            self._add_item_board(self.objects["snake"], coordinate)
 
-    def add_apple(self):
-        pass
+    def add_apple(self, apple: Apple) -> bool:
+        if len(self.objects["apples"]) >= 3:
+            return False
+        # x, y, score = get_random_apple_data()
+        # apple = Apple((x, y), score)
+        if not self.cell_content(apple.get_location()):
+            return False
+        else:
+            self.objects["apple"].append(apple)
 
-    def add_car(self):
-        pass
+    def add_bomb(self, bomb: Bomb):
+        if not self.objects["bombs"] or not self.objects["explosion"]:
+            return False
+        # x, y, radius, time = get_random_bomb_data()
+        # bomb = Bomb((x,y), radius, time)
+        if not self.cell_content(bomb.get_location()):
+            return False
+        else:
+            self.objects["bomb"] = bomb
